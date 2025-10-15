c172e23e5743bb07d9fd9c870f8d6afe56893e3b
lothixoa04022002 <lothixoa04022002@gmail.com>
2025-10-15 21:13:41 +0700

    Update translate.ts

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -1,41 +1,46 @@
 import axios from 'axios';
 
 const translateText = async (text: string, targetLang: string = ''): Promise<string> => {
-    try {
-        const targetLangs = {
-            KR: 'ko',
-            JP: 'ja',
-            CN: 'zh',
-            SA: 'ar',
-            BR: 'pt-BR',
-            FR: 'fr',
-            IT: 'it',
-            US: 'en',
-            ID: 'id',
-            DE: 'de',
-            VN: 'vi',
-            TH: 'th'
-            SE: 'sv'
-        };
-        if (!targetLangs[targetLang as keyof typeof targetLangs]) {
-            return text;
-        }
-        const encodedText = encodeURIComponent(text);
-        const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=${targetLangs[targetLang as keyof typeof targetLangs]}&dt=t&q=${encodedText}`;
+  try {
+    const targetLangs = {
+      KR: 'ko',
+      JP: 'ja',
+      CN: 'zh',
+      SA: 'ar',
+      BR: 'pt-BR',
+      FR: 'fr',
+      IT: 'it',
+      US: 'en',
+      ID: 'id',
+      DE: 'de',
+      VN: 'vi',
+      TH: 'th',
+      SE: 'sv', // Thụy Điển
+    };
 
-        const response = await axios.get(url);
-        const data: unknown = response.data;
-
-        if (Array.isArray(data) && Array.isArray(data[0])) {
-            const translatedParts = data[0] as Array<[string, string, unknown, unknown, unknown, unknown, unknown, unknown, unknown]>;
-            const translatedText = translatedParts.map((part) => part[0]).join('');
-            return translatedText.trim();
-        }
-        console.clear();
-        return text;
-    } catch {
-        console.clear();
-        return text;
+    if (!targetLangs[targetLang as keyof typeof targetLangs]) {
+      return text;
     }
+
+    const encodedText = encodeURIComponent(text);
+    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=${
+      targetLangs[targetLang as keyof typeof targetLangs]
+    }&dt=t&q=${encodedText}`;
+
+    const response = await axios.get(url);
+    const data: unknown = response.data;
+
+    if (Array.isArray(data) && Array.isArray(data[0])) {
+      const translatedParts = data[0] as Array<[string]>;
+      const translatedText = translatedParts.map((part) => part[0]).join('');
+      return translatedText.trim();
+    }
+
+    return text;
+  } catch (error) {
+    console.error('Translation error:', error);
+    return text;
+  }
 };
+
 export default translateText;
